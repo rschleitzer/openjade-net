886554033f1b6c43335080ae1c68c94c357ed846
ralf <ralf@scaly.io>
2025-12-03 07:34:50 +0100

    Add ParserMessages for parsing methods

--- src/OpenSP/ParserMessages.cs
+++ src/OpenSP/ParserMessages.cs
@@ -138,4 +138,80 @@ public static class ParserMessages
         MessageType.Severity.error, null, 209, "cannot continue with subdocument");
     public static readonly MessageType0 giveUp = new MessageType0(
         MessageType.Severity.error, null, 210, "cannot continue");
+
+    // parseCommon messages
+    public static readonly MessageType0 processingInstructionEntityEnd = new MessageType0(
+        MessageType.Severity.error, null, 211, "entity ended within processing instruction");
+    public static readonly MessageType1 processingInstructionLength = new MessageType1(
+        MessageType.Severity.error, null, 212, "length of processing instruction exceeds %1");
+    public static readonly MessageType0 processingInstructionClose = new MessageType0(
+        MessageType.Severity.error, null, 213, "missing PIC delimiter");
+    public static readonly MessageType0 piMissingName = new MessageType0(
+        MessageType.Severity.warning, null, 214, "processing instruction does not start with name");
+    public static readonly MessageType0 literalLevel = new MessageType0(
+        MessageType.Severity.error, null, 215, "literal closed by entity end in different entity");
+    public static readonly MessageType1 literalMinimumData = new MessageType1(
+        MessageType.Severity.error, null, 216, "'%1' invalid in minimum data");
+    public static readonly MessageType0 namedCharRef = new MessageType0(
+        MessageType.Severity.warning, null, 217, "named character reference");
+    public static readonly MessageType1 functionName = new MessageType1(
+        MessageType.Severity.error, null, 218, "'%1' is not a function name");
+    public static readonly MessageType0 refc = new MessageType0(
+        MessageType.Severity.warning, null, 219, "missing REFC delimiter");
+    public static readonly MessageType1 characterNumber = new MessageType1(
+        MessageType.Severity.error, null, 220, "'%1' is not a valid character number");
+    public static readonly MessageType1 hexNumberLength = new MessageType1(
+        MessageType.Severity.error, null, 221, "hex number length exceeds %1");
+    public static readonly MessageType0 nonSgmlCharRef = new MessageType0(
+        MessageType.Severity.warning, null, 222, "numeric character reference to non-SGML character");
+    public static readonly MessageType1 numericCharRefLiteralNonSgml = new MessageType1(
+        MessageType.Severity.error, null, 223, "numeric character reference to %1 not allowed in literal");
+    public static readonly MessageType0 dataTagPatternNonSgml = new MessageType0(
+        MessageType.Severity.error, null, 224, "numeric character reference to non-SGML character in data tag pattern");
+    public static readonly MessageType0 dataTagPatternFunction = new MessageType0(
+        MessageType.Severity.error, null, 225, "numeric character reference to function character in data tag pattern");
+    public static readonly MessageType0 eroGrpoStartTag = new MessageType0(
+        MessageType.Severity.error, null, 226, "ERO GRPO not allowed in start tag");
+    public static readonly MessageType0 eroGrpoProlog = new MessageType0(
+        MessageType.Severity.error, null, 227, "ERO GRPO not allowed in prolog");
+    public static readonly MessageType0 internalSubsetLiteralParamEntityRef = new MessageType0(
+        MessageType.Severity.warning, null, 228, "parameter entity reference in internal subset literal");
+    public static readonly MessageType0 peroGrpoProlog = new MessageType0(
+        MessageType.Severity.error, null, 229, "PERO GRPO not allowed in prolog");
+    public static readonly MessageType1 dataCharDelim = new MessageType1(
+        MessageType.Severity.warning, null, 230, "'%1' is first character of delimiter in content");
+    public static readonly MessageType0 entityReferenceMissingName = new MessageType0(
+        MessageType.Severity.error, null, 231, "missing entity name after entity reference delimiter");
+    public static readonly MessageType1 parameterEntityNameLength = new MessageType1(
+        MessageType.Severity.error, null, 232, "parameter entity name length exceeds %1");
+    public static readonly MessageType1 nameLength = new MessageType1(
+        MessageType.Severity.error, null, 233, "name length exceeds %1");
+    public static readonly MessageType1 entityUndefined = new MessageType1(
+        MessageType.Severity.error, null, 234, "entity '%1' not defined");
+    public static readonly MessageType1 parameterEntityUndefined = new MessageType1(
+        MessageType.Severity.error, null, 235, "parameter entity '%1' not defined");
+    public static readonly MessageType0 entityApplicableDtd = new MessageType0(
+        MessageType.Severity.error, null, 236, "no applicable DTD");
+    public static readonly MessageType1 defaultEntityReference = new MessageType1(
+        MessageType.Severity.warning, null, 237, "reference to default entity '%1'");
+    public static readonly MessageType1L commentEntityEnd = new MessageType1L(
+        MessageType.Severity.error, null, 238, "comment closed by entity end started here");
+    public static readonly MessageType1 sdCommentSignificant = new MessageType1(
+        MessageType.Severity.error, null, 239, "'%1' invalid in SGML declaration comment");
+    public static readonly MessageType1 numberLength = new MessageType1(
+        MessageType.Severity.error, null, 240, "number length exceeds %1");
+    public static readonly MessageType1 nonSgmlCharacter = new MessageType1(
+        MessageType.Severity.error, null, 241, "non-SGML character %1");
+    public static readonly MessageType2 numericCharRefUnknownDesc = new MessageType2(
+        MessageType.Severity.error, null, 242, "character %1 has unknown description '%2'");
+    public static readonly MessageType3 numericCharRefUnknownBase = new MessageType3(
+        MessageType.Severity.error, null, 243, "character %1 unknown in base character set %3 at position %2");
+    public static readonly MessageType1 numericCharRefBadInternal = new MessageType1(
+        MessageType.Severity.error, null, 244, "character %1 cannot be represented in internal character set");
+    public static readonly MessageType1 numericCharRefNoInternal = new MessageType1(
+        MessageType.Severity.error, null, 245, "no internal representation for character %1");
+
+    // parseMode messages
+    public static readonly MessageType2 lexicalAmbiguity = new MessageType2(
+        MessageType.Severity.warning, null, 246, "lexical ambiguity between '%1' and '%2'");
 }
