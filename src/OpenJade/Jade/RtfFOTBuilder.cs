f029813ca3a233d35384cef5bcdf9ea674bbd55c
ralf <ralf@scaly.io>
2025-12-10 21:34:12 +0100

    Fix RTF character encoding: implement Windows-1252 mapping with hexChar output

--- src/OpenJade/Jade/RtfFOTBuilder.cs
+++ src/OpenJade/Jade/RtfFOTBuilder.cs
@@ -68,6 +68,43 @@ public class RtfFOTBuilder : FOTBuilder
     // Color table
     private System.Collections.Generic.List<DeviceRGBColor> colorTable_;
 
+    // Windows-1252 (CP1252) Unicode to byte mapping for characters 0x80-0x9F
+    // These are the non-standard characters that differ from ISO-8859-1
+    private static readonly System.Collections.Generic.Dictionary<uint, byte> win1252Map_ = new()
+    {
+        // Row 0x80-0x8F
+        { 0x201a, 0x82 }, // Single Low-9 Quotation Mark
+        { 0x0192, 0x83 }, // Latin Small Letter F With Hook
+        { 0x201e, 0x84 }, // Double Low-9 Quotation Mark
+        { 0x2026, 0x85 }, // Horizontal Ellipsis
+        { 0x2020, 0x86 }, // Dagger
+        { 0x2021, 0x87 }, // Double Dagger
+        { 0x02c6, 0x88 }, // Modifier Letter Circumflex Accent
+        { 0x2030, 0x89 }, // Per Mille Sign
+        { 0x0160, 0x8a }, // Latin Capital Letter S With Caron
+        { 0x2039, 0x8b }, // Single Left-Pointing Angle Quotation Mark
+        { 0x0152, 0x8c }, // Latin Capital Ligature OE
+        { 0x017d, 0x8e }, // Latin Capital Letter Z With Caron
+        // Row 0x90-0x9F
+        { 0x2018, 0x91 }, // Left Single Quotation Mark
+        { 0x2019, 0x92 }, // Right Single Quotation Mark
+        { 0x201c, 0x93 }, // Left Double Quotation Mark
+        { 0x201d, 0x94 }, // Right Double Quotation Mark
+        { 0x2022, 0x95 }, // Bullet
+        { 0x2013, 0x96 }, // En Dash
+        { 0x2014, 0x97 }, // Em Dash
+        { 0x02dc, 0x98 }, // Small Tilde
+        { 0x2122, 0x99 }, // Trade Mark Sign
+        { 0x0161, 0x9a }, // Latin Small Letter S With Caron
+        { 0x203a, 0x9b }, // Single Right-Pointing Angle Quotation Mark
+        { 0x0153, 0x9c }, // Latin Small Ligature OE
+        { 0x017e, 0x9e }, // Latin Small Letter Z With Caron
+        { 0x0178, 0x9f }, // Latin Capital Letter Y With Diaeresis
+    };
+
+    // Hex digits for RTF hex escapes
+    private static readonly char[] hexDigits_ = "0123456789abcdef".ToCharArray();
+
     public enum InlineState
     {
         inlineFirst,
@@ -454,22 +491,37 @@ public class RtfFOTBuilder : FOTBuilder
             {
                 os("\\line ");
             }
-            else if (c < 128)
+            else if (c < 0x80)
             {
+                // ASCII characters output directly
                 os(((char)c).ToString());
             }
-            else if (c < 256)
+            else if (c >= 0xa0 && c <= 0xff)
             {
-                os("\\'");
-                os(((int)c).ToString("x2"));
+                // Latin-1 Supplement (U+00A0-U+00FF) maps directly to Windows-1252
+                hexChar(c);
+            }
+            else if (win1252Map_.TryGetValue(c, out byte win1252Code))
+            {
+                // Character has a Windows-1252 mapping
+                hexChar(win1252Code);
             }
             else
             {
+                // Unicode escape with '?' fallback character
                 os("\\u");
-                os((int)c);
-                os("?");
+                os((int)(short)c);
+                hexChar('?');
             }
         }
+    }
+
+    // Output character as RTF hex escape \'XX
+    private void hexChar(uint code)
+    {
+        os("\\'");
+        os(hexDigits_[(code >> 4) & 0xf].ToString());
+        os(hexDigits_[code & 0xf].ToString());
     }
 
     private void syncCharFormat()
